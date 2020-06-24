3d9ed56301c9049be6ffceccb3593934cdd4febf
Artem <arediveev@gmail.com>
2020-06-24 18:54:43 +0300

    Initial commit

---
+++ extraChat.lua
@@ -0,0 +1,12 @@
+script_name("ExtraChat")
+script_author("TrenLok")
+script_description("Display messages by key words in the extra chat")
+
+function main()
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+
+    while true do
+        wait(0)
+    end
+end
